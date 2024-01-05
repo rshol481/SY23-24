a5733f9ba44ffa5224646cf69be20ef7435ea022
Robbie <rshol481@learner.dvusd.org>
2024-01-05 09:29:09 -0700

    stage 3
    1-100 for loop

--- converter/converter/Program.cs
+++ converter/converter/Program.cs
@@ -10,6 +10,11 @@ namespace converter
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
