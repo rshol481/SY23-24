f45e1cd1f2e230b661c03a9b155654fb466e6cbe
Robbie <rshol481@learner.dvusd.org>
2024-01-10 09:22:35 -0700

    Stage 6
    converter and decoder programs

--- converter/converter/Program.cs
+++ converter/converter/Program.cs
@@ -10,12 +10,52 @@ namespace converter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter The Number");
+            Console.WriteLine("enter the number of liters");
+            int liters = 0;
+            int.TryParse(Console.ReadLine(), out liters);
+            Console.WriteLine("enter 1 for fl oz \nenter 2 for gi \nenter 3 for pt \nenter 4 for qt \nenter 5 for gal \nenter 6 for tbsp \nenter 7 for tsp \nenter 8 for cup");
             int num = 0;
             int.TryParse(Console.ReadLine(), out num);
-            Console.WriteLine(num + 2);
+            //if(num == 1)
+            //{
+              //  Console.WriteLine(meters * 39 / 12 + "feet");
+              //  Console.ReadLine();
+            //}               
+            //if (num == 2)
+            //{
+              //  Console.WriteLine(meters * 39 + "yards");
+               // Console.ReadLine();
+            //}   
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine(liters * 33.814 + "fl oz");
+                    break;
+                case 2:
+                    Console.WriteLine(liters * 8.45351 + "gills");
+                    break;
+                case 3:
+                    Console.WriteLine(liters * 2.113 + "pt");
+                    break;
+                case 4:
+                    Console.WriteLine(liters * 1.057 + "qt");
+                    break;
+                case 5:
+                    Console.WriteLine(liters / 3.785 + "gal");
+                    break;
+                case 6:
+                    Console.WriteLine(liters * 67.628 + "tbsp");
+                    break;
+                case 7:
+                    Console.WriteLine(liters * 202.9 + "tsp");
+                    break;
+                case 8:
+                    Console.WriteLine(liters * 4.167 + "cups");
+                    break;
+                default:
+                    break;
+            }
             Console.ReadLine();
-
         }
     }
 }
---
+++ decoder/decoder/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace decoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'a')
+                label1.Text += "1";
+            else
+                label1.Text += e.KeyChar.ToString();
+            switch (e.KeyChar)
+            {
+                case 'b':
+                    label1.Text += '2';
+                break;
+                case 'c':
+                    label1.Text += '3';
+                    break;
+                case 'd':
+                    label1.Text += '4';
+                    break;
+                case 'e':
+                    label1.Text += '5';
+                    break;
+                case 'f':
+                    label1.Text += '6';
+                    break;
+                case 'g':
+                    label1.Text += '7';
+                    break;
+                case 'h':
+                    label1.Text += '8';
+                    break;
+                case 'i':
+                    label1.Text += '9';
+                    break;
+                case 'j':
+                    label1.Text += '0';
+                    break;
+                case 'k':
+                    label1.Text += 'a';
+                    break;
+                case 'l':
+                    label1.Text += 'b';
+                    break;
+                case 'm':
+                    label1.Text += 'c';
+                    break;
+                case 'n':
+                    label1.Text += 'd';
+                    break;
+                case 'o':
+                    label1.Text += 'e';
+                    break;
+                case 'p':
+                    label1.Text += 'f';
+                    break;
+                case 'q':
+                    label1.Text += 'g';
+                    break;
+                case 'r':
+                    label1.Text += 'h';
+                    break;
+                case 's':
+                    label1.Text += 'i';
+                    break;
+                case 't':
+                    label1.Text += 'j';
+                    break;
+                case 'u':
+                    label1.Text += 'k';
+                    break;
+                case 'v':
+                    label1.Text += 'l';
+                    break;
+                case 'w':
+                    label1.Text += 'm';
+                    break;
+                case 'x':
+                    label1.Text += 'n';
+                    break;
+                case 'y':
+                    label1.Text += 'o';
+                    break;
+                case 'z':
+                    label1.Text += 'p';
+                    break;
+                    default:
+                    break;
+            }
+        }
+    }
+}
